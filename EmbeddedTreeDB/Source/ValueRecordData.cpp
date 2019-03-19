dc5d344a0aeb3c05e3eb816ab308d9b8c33610d9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-03-19 23:39:14 +0000

    Updates

--- EmbeddedTreeDB/Source/ValueRecordData.cpp
+++ EmbeddedTreeDB/Source/ValueRecordData.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2018 Xavier Leclercq
+    Copyright (c) 2018-2019 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -27,14 +27,12 @@ namespace DiplodocusDB
 {
 
 ValueRecordData::ValueRecordData()
-    : RecordData(Record::ERecordType::eValue),
-    m_type(EPrimitiveDataType::eNULL)
+    : m_type(EPrimitiveDataType::eNULL)
 {
 }
 
 ValueRecordData::ValueRecordData(const TreeDBValue& value)
-    : RecordData(Record::ERecordType::eValue),
-    m_type(value.type()), m_buffer(value.asString())
+    : m_type(value.type()), m_buffer(value.asString())
 {
 }
 
