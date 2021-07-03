732f3104afa079e47edcb00524da3dee467a1ae0
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-03 16:23:53 +0100

    Refactoring (#39)

--- Tests/Core/Source/main.cpp
+++ Tests/Core/Source/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2018-2019 Xavier Leclercq
+    Copyright (c) 2018-2021 Xavier Leclercq
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -22,7 +22,7 @@
 
 #include "TreeDBKeyTests.h"
 #include "TreeDBValueTests.h"
-#include "Ishiko/TestFramework/TestFrameworkCore.h"
+#include <Ishiko/Tests.h>
 
 using namespace Ishiko::Tests;
 
