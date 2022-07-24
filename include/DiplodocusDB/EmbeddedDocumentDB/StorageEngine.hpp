d00426431f61053fc8e3981b4854edba52ab7f06
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-24 14:48:37 +0100

    Add file

---
+++ include/DiplodocusDB/EmbeddedDocumentDB/StorageEngine.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/diplodocusdb/embedded-document-db/blob/main/LICENSE.txt
+*/
+
+#ifndef _DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_HPP_
+#define _DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_HPP_
+
+#endif
