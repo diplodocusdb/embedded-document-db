e2ef7a6b6e7fa87022b3d7cceef47afa48421d2a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-25 20:16:26 +0100

    Fix StorageEngine tests (#111)

--- include/DiplodocusDB/EmbeddedDocumentDB/StorageEngine/Record.hpp
+++ include/DiplodocusDB/EmbeddedDocumentDB/StorageEngine/Record.hpp
@@ -132,16 +132,6 @@ public:
     void write(PageRepositoryWriter& writer, Ishiko::Error& error) const;
 
 private:
-    static Value readInlineValue(PageRepositoryReader& reader, Ishiko::Error& error);
-    static void writeInlineValue(PageRepositoryWriter& writer, const Value& value, Ishiko::Error& error);
-    static DataType readDataType(PageRepositoryReader& reader, Ishiko::Error& error);
-    static void writeDataType(PageRepositoryWriter& writer, const DataType& dataType, Ishiko::Error& error);
-    static bool readBoolean(PageRepositoryReader& reader, Ishiko::Error& error);
-    static void writeBoolean(PageRepositoryWriter& writer, bool data, Ishiko::Error& error);
-    static std::string readString(PageRepositoryReader& reader, Ishiko::Error& error);
-    static void writeString(PageRepositoryWriter& writer, const std::string& data, Ishiko::Error& error);
-
-private:
     Record::ERecordType m_type;
     boost::variant<MasterFileMetadata, NodeID, std::string, Value> m_data;
 };
---
+++ include/DiplodocusDB/EmbeddedDocumentDB/StorageEngine/ValueCodec.hpp
@@ -0,0 +1,32 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/diplodocusdb/embedded-document-db/blob/main/LICENSE.txt
+*/
+
+#ifndef _DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_VALUECODEC_HPP_
+#define _DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_VALUECODEC_HPP_
+
+#include <DiplodocusDB/Core.hpp>
+#include <DiplodocusDB/PhysicalStorage/PageRepository.hpp>
+#include <Ishiko/Errors.hpp>
+
+namespace DiplodocusDB
+{
+
+class ValueCodec
+{
+public:
+    static Value ReadInlineValue(PageRepositoryReader& reader, Ishiko::Error& error);
+    static void WriteInlineValue(PageRepositoryWriter& writer, const Value& value, Ishiko::Error& error);
+    static DataType ReadDataType(PageRepositoryReader& reader, Ishiko::Error& error);
+    static void WriteDataType(PageRepositoryWriter& writer, const DataType& dataType, Ishiko::Error& error);
+    static bool ReadBoolean(PageRepositoryReader& reader, Ishiko::Error& error);
+    static void WriteBoolean(PageRepositoryWriter& writer, bool data, Ishiko::Error& error);
+    static std::string ReadString(PageRepositoryReader& reader, Ishiko::Error& error);
+    static void WriteString(PageRepositoryWriter& writer, const std::string& data, Ishiko::Error& error);
+};
+
+}
+
+#endif
