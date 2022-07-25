f2ab2c7553c02e04a6cfd377d19e81baf960d548
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-25 21:20:09 +0100

    Remove obsolete files

--- database/src/XMLTreeDBTransactionImpl.cpp
+++
@@ -1,7 +0,0 @@
-/*
-    Copyright (c) 2019-2022 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/diplodocusdb/tree-db/blob/main/LICENSE.txt
-*/
-
-#include "XMLTreeDBTransactionImpl.hpp"
