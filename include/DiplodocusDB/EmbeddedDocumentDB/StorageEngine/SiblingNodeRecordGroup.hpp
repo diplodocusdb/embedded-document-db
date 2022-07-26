d6043eaec163a742e84bd7b2d7787d9eab0c55b4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-26 19:46:27 +0100

    Updates

--- include/DiplodocusDB/EmbeddedDocumentDB/StorageEngine/SiblingNodeRecordGroup.hpp
+++ include/DiplodocusDB/EmbeddedDocumentDB/StorageEngine/SiblingNodeRecordGroup.hpp
@@ -7,4 +7,32 @@
 #ifndef _DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_SIBLINGNODERECORDGROUP_HPP_
 #define _DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_SIBLINGNODERECORDGROUP_HPP_
 
+#include "NodeID.hpp"
+#include <DiplodocusDB/Core.hpp>
+#include <string>
+
+namespace DiplodocusDB
+{
+
+class SiblingNodeRecordGroup
+{
+public:
+    SiblingNodeRecordGroup();
+    SiblingNodeRecordGroup(std::string name, NodeID nodeID);
+    SiblingNodeRecordGroup(std::string name, Value value, NodeID nodeID);
+
+    const std::string& name() const;
+    const Value& value() const;
+    Value& value();
+    bool isRoot() const;
+    const NodeID& nodeID() const;
+
+private:
+    std::string m_name;
+    Value m_value;
+    NodeID m_nodeID;
+};
+
+}
+
 #endif
--- storage-engine/src/SiblingNodeRecordGroup.cpp
+++ storage-engine/src/SiblingNodeRecordGroup.cpp
@@ -5,3 +5,44 @@
 */
 
 #include "SiblingNodeRecordGroup.hpp"
+
+using namespace DiplodocusDB;
+
+SiblingNodeRecordGroup::SiblingNodeRecordGroup()
+{
+}
+
+SiblingNodeRecordGroup::SiblingNodeRecordGroup(std::string name, NodeID nodeID)
+    : m_name(std::move(name)), m_nodeID(std::move(nodeID))
+{
+}
+
+SiblingNodeRecordGroup::SiblingNodeRecordGroup(std::string name, Value value, NodeID nodeID)
+    : m_name(std::move(name)), m_value(std::move(value)), m_nodeID(std::move(nodeID))
+{
+}
+
+const std::string& SiblingNodeRecordGroup::name() const
+{
+    return m_name;
+}
+
+const Value& SiblingNodeRecordGroup::value() const
+{
+    return m_value;
+}
+
+Value& SiblingNodeRecordGroup::value()
+{
+    return m_value;
+}
+
+bool SiblingNodeRecordGroup::isRoot() const
+{
+    return (m_name == "/");
+}
+
+const NodeID& SiblingNodeRecordGroup::nodeID() const
+{
+    return m_nodeID;
+}
