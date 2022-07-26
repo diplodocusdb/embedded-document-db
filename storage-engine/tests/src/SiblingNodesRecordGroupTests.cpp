43215149105e38d1e53e74c98d16de8d530fdf6a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-26 21:40:40 +0100

    Move files

--- storage-engine/tests/src/SiblingNodesRecordGroupTests.cpp
+++ storage-engine/tests/src/SiblingNodesRecordGroupTests.cpp
@@ -1,28 +1,12 @@
 /*
     Copyright (c) 2019-2022 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Released under the MIT License
+    See https://github.com/diplodocusdb/embedded-document-db/blob/main/LICENSE.txt
 */
 
-#include "SiblingNodesRecordGroupTests.h"
-#include "DiplodocusDB/EmbeddedDocumentDB/SiblingNodesRecordGroup.h"
-#include "DiplodocusDB/PhysicalStorage/PageRepository/PageFileRepository.h"
+#include "SiblingNodesRecordGroupTests.hpp"
+#include "DiplodocusDB/EmbeddedDocumentDB/StorageEngine/SiblingNodesRecordGroup.hpp"
+#include <DiplodocusDB/PhysicalStorage/PageRepository.hpp>
 
 using namespace DiplodocusDB;
 using namespace Ishiko;
@@ -39,7 +23,7 @@ SiblingNodesRecordGroupTests::SiblingNodesRecordGroupTests(const TestNumber& num
 
 void SiblingNodesRecordGroupTests::ConstructionTest1(Test& test)
 {
-    DiplodocusDB::SiblingNodesRecordGroup siblingNodesRecordGroup;
+    SiblingNodesRecordGroup siblingNodesRecordGroup;
 
     ISHIKO_TEST_PASS();
 }
@@ -50,25 +34,26 @@ void SiblingNodesRecordGroupTests::WriteTest1(Test& test)
     
     Error error;
 
-    DiplodocusDB::PageFileRepository repository;
+    PageFileRepository repository;
     repository.create(test.context().getOutputPath(testName), error);
 
     ISHIKO_TEST_ABORT_IF(error);
 
-    std::shared_ptr<DiplodocusDB::Page> page = repository.allocatePage(error);
+    std::shared_ptr<Page> page = repository.allocatePage(error);
 
     ISHIKO_TEST_ABORT_IF(error);
 
-    DiplodocusDB::PageRepositoryWriter writer = repository.insert(page, 0, error);
+    PageRepositoryWriter writer = repository.insert(page, 0, error);
 
     ISHIKO_TEST_ABORT_IF(error);
 
-    DiplodocusDB::SiblingNodesRecordGroup siblingNodesRecordGroup;
+    SiblingNodesRecordGroup siblingNodesRecordGroup;
     siblingNodesRecordGroup.write(writer, error);
 
     ISHIKO_TEST_FAIL_IF(error);
 
     repository.save(*page, error);
+    repository.close();
 
     ISHIKO_TEST_FAIL_IF(error);
     ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(testName);
@@ -81,7 +66,7 @@ void SiblingNodesRecordGroupTests::WriteTest2(Test& test)
 
     Error error;
 
-    DiplodocusDB::PageFileRepository repository;
+    PageFileRepository repository;
     repository.create(test.context().getOutputPath(testName), error);
 
     ISHIKO_TEST_ABORT_IF(error);
@@ -102,6 +87,7 @@ void SiblingNodesRecordGroupTests::WriteTest2(Test& test)
     ISHIKO_TEST_FAIL_IF(error);
 
     repository.save(*page, error);
+    repository.close();
 
     ISHIKO_TEST_FAIL_IF(error);
     ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(testName);
@@ -135,6 +121,7 @@ void SiblingNodesRecordGroupTests::WriteTest3(Test& test)
     ISHIKO_TEST_FAIL_IF(error);
 
     repository.save(*page, error);
+    repository.close();
 
     ISHIKO_TEST_FAIL_IF(error);
     ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(testName);
@@ -170,6 +157,7 @@ void SiblingNodesRecordGroupTests::WriteTest4(Test& test)
     ISHIKO_TEST_FAIL_IF(error);
 
     repository.save(*page, error);
+    repository.close();
 
     ISHIKO_TEST_FAIL_IF(error);
     ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(testName);
