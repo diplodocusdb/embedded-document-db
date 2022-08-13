01720ecf293ca1406414e4678b46df64531a48b5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-08-13 13:51:21 +0100

    Add tests (#131)

---
+++ storage-engine/tests/src/RecordFileTests.hpp
@@ -0,0 +1,27 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/diplodocusdb/embedded-document-db/blob/main/LICENSE.txt
+*/
+
+#ifndef GUARD_DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_TESTS_RECORDFILETESTS_HPP
+#define GUARD_DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_TESTS_RECORDFILETESTS_HPP
+
+#include <Ishiko/TestFramework.hpp>
+
+class RecordFileTests : public Ishiko::TestSequence
+{
+public:
+    RecordFileTests(const Ishiko::TestNumber& number, const Ishiko::TestContext& context);
+
+private:
+    static void CreationTest1(Ishiko::Test& test);
+    static void CreateTest1(Ishiko::Test& test);
+    static void OpenTest1(Ishiko::Test& test);
+    static void OpenTest2(Ishiko::Test& test);
+    static void AllocatePageTest1(Ishiko::Test& test);
+    static void AllocatePageTest2(Ishiko::Test& test);
+    static void InsertPageAfterTest1(Ishiko::Test& test);
+};
+
+#endif
