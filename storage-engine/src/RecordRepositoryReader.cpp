292ec39829ccef14b56a5f1b272c12214033da2d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-08-13 12:41:43 +0100

    Updates

--- storage-engine/src/RecordRepositoryReader.cpp
+++ storage-engine/src/RecordRepositoryReader.cpp
@@ -1,7 +1,7 @@
 /*
     Copyright (c) 2018-2022 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/diplodocusdb/physical-storage/blob/main/LICENSE.txt
+    See https://github.com/diplodocusdb/embedded-document-db/blob/main/LICENSE.txt
 */
 
 #include "RecordRepositoryReader.hpp"
