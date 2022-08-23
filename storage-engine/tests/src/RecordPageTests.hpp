3927239712438f7eae060f9a5571ff8ebf3a4143
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-08-23 22:20:29 +0100

    Add test files

---
+++ storage-engine/tests/src/RecordPageTests.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/diplodocusdb/embedded-document-db/blob/main/LICENSE.txt
+*/
+
+#ifndef GUARD_DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_TESTS_RECORDPAGETESTS_HPP
+#define GUARD_DIPLODOCUSDB_EMBEDDEDDOCUMENTDB_STORAGEENGINE_TESTS_RECORDPAGETESTS_HPP
+
+#endif
