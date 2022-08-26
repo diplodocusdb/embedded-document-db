fd942929495db23f80cc5baebabb15b33805efce
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-08-26 14:12:55 +0100

    Update tests

--- storage-engine/tests/src/RecordFileTests.hpp
+++ storage-engine/tests/src/RecordFileTests.hpp
@@ -15,7 +15,7 @@ public:
     RecordFileTests(const Ishiko::TestNumber& number, const Ishiko::TestContext& context);
 
 private:
-    static void CreationTest1(Ishiko::Test& test);
+    static void ConstructorTest1(Ishiko::Test& test);
     static void CreateTest1(Ishiko::Test& test);
     static void OpenTest1(Ishiko::Test& test);
     static void OpenTest2(Ishiko::Test& test);
